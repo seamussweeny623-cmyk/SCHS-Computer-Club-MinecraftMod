1a30f6b48232e0414d59435e48f2c733ea696f2b
Seamus <seamussweeny623@gmail.com>
2026-02-16 14:43:54 -0800

    "tutorialmod" -> "mediterraneanmod"

--- src/main/java/net/seamus/mediterraneanmod/MediterraneanMod.java
+++ src/main/java/net/seamus/mediterraneanmod/MediterraneanMod.java
@@ -1,4 +1,4 @@
-package net.seamus.tutorialmod;
+package net.seamus.mediterraneanmod;
 
 import com.mojang.logging.LogUtils;
 import net.minecraftforge.api.distmarker.Dist;
@@ -16,15 +16,15 @@ import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.slf4j.Logger;
 
 // The value here should match an entry in the META-INF/mods.toml file
-@Mod(TutorialMod.MOD_ID)
-public class TutorialMod {
+@Mod(MediterraneanMod.MOD_ID)
+public class MediterraneanMod {
     // Define mod id in a common place for everything to reference
-    public static final String MOD_ID = "tutorialmod";
+    public static final String MOD_ID = "mediterranean_schs";
     // Directly reference a slf4j logger
     public static final Logger LOGGER = LogUtils.getLogger();
 
 
-    public TutorialMod() {
+    public MediterraneanMod() {
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
         modEventBus.addListener(this::commonSetup);
 
